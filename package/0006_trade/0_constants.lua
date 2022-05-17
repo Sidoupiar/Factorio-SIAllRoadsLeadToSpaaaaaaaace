747e43917c34192f907cd245107ece1f4dc46ff4
Sidoupiar <sidoupiar@qq.com>
2022-05-17 10:36:24 +0800

    修改基础结构

--- package/0006_trade/0_constants.lua
+++ package/0006_trade/0_constants.lua
@@ -1,6 +1,21 @@
 local constants =
 {
 	id = "Trade" ,
-	name = "经济贸易"
+	name = "经济贸易" ,
+	raw =
+	{
+		coin =
+		{
+			type = SITypes.item.capsule ,
+			list =
+			{
+				coin = "硬币" ,
+				gold = "金币" ,
+				balla = "贝拉" ,
+				innew = "因纽" ,
+				charp = "叉普"
+			}
+		}
+	}
 }
 return constants
---
+++ package/0007_mission/0_constants.lua
@@ -0,0 +1,6 @@
+local constants =
+{
+	id = "Mission" ,
+	name = "支线任务"
+}
+return constants
