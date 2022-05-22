49567a29a74f2261b55d6f26335151fba8e40708
Sidoupiar <sidoupiar@qq.com>
2022-05-22 15:43:27 +0800

    创建多级蓄电器

--- package/0009_base_improvement/1_data.lua
+++ package/0009_base_improvement/1_data.lua
@@ -4,6 +4,59 @@
 
 if not SIMods.base.loaded then return end
 
+local function accumulator_picture( tint , repeat_count , other )
+	local layers =
+	{
+		layers =
+		{
+			{
+				filename = "__base__/graphics/entity/accumulator/accumulator.png" ,
+				priority = "high" ,
+				width = 66 ,
+				height = 94 ,
+				shift = util.by_pixel( 0 , -10 ) ,
+				repeat_count = repeat_count ,
+				tint = tint ,
+				animation_speed = 0.5 ,
+				hr_version =
+				{
+					filename = "__base__/graphics/entity/accumulator/hr-accumulator.png" ,
+					priority = "high" , 
+					width = 130 , 
+					height = 189 , 
+					shift = util.by_pixel( 0 , -11 ) ,
+					repeat_count = repeat_count ,
+					tint = tint ,
+					animation_speed = 0.5 ,
+					scale = 0.5
+				}
+			} ,
+			{
+				filename = "__base__/graphics/entity/accumulator/accumulator-shadow.png" ,
+				priority = "high" ,
+				width = 120 ,
+				height = 54 ,
+				shift = util.by_pixel( 28 , 6 ) ,
+				repeat_count = repeat_count ,
+				draw_as_shadow = true ,
+				hr_version =
+				{
+					filename = "__base__/graphics/entity/accumulator/hr-accumulator-shadow.png" ,
+					priority = "high" ,
+					width = 234 ,
+					height = 106 ,
+					shift = util.by_pixel( 29 , 6 ) ,
+					repeat_count = repeat_count ,
+					draw_as_shadow = true ,
+					scale = 0.5
+				}
+			}
+		}
+	}
+	for index , layer in pairs( other or {} ) do table.insert( layers.layers , layer ) end
+	return layers
+end
+
 local nameList =
 {
 	base     = { name = "基础" } ,
@@ -23,7 +76,14 @@ local solarData =
 	max_health = 200 ,
 	corpse = "solar-panel-remnants" ,
 	dying_explosion = "solar-panel-explosion" ,
-	damaged_trigger_effect = hit_effects.entity() ,
+	damaged_trigger_effect =
+	{
+		type = "create-entity" ,
+		entity_name = "spark-explosion-higher" ,
+		offset_deviation = { { -0.5 , -0.5 } , { 0.5 , 0.5 } } ,
+		offsets = { { 0 , 1.5 } } ,
+		damage_type_filters = "fire"
+	} ,
 	energy_source =
 	{
 		type = "electric" ,
@@ -61,18 +121,87 @@ local accData =
 	max_health = 200 ,
 	corpse = "accumulator-remnants" ,
 	dying_explosion = "accumulator-explosion" ,
-	damaged_trigger_effect = hit_effects.entity() ,
+	damaged_trigger_effect =
+	{
+		type = "create-entity" ,
+		entity_name = "spark-explosion-higher" ,
+		offset_deviation = { { -0.5 , -0.5 } , { 0.5 , 0.5 } } ,
+		offsets = { { 0 , 1.5 } } ,
+		damage_type_filters = "fire"
+	} ,
 	energy_source =
 	{
 		type = "electric" ,
 		usage_priority = "tertiary"
 	} ,
 	picture = accumulator_picture() ,
-	water_reflection = accumulator_reflection() ,
-	charge_animation = accumulator_charge() ,
+	water_reflection =
+	{
+		pictures =
+		{
+			filename = "__base__/graphics/entity/accumulator/accumulator-reflection.png" ,
+			priority = "extra-high" ,
+			width = 20 ,
+			height = 24 ,
+			shift = util.by_pixel( 0 , 50 ) ,
+			variation_count = 1 ,
+			scale = 5
+		} ,
+		rotate = false ,
+		orientation_to_variation = false
+	} ,
+	charge_animation = accumulator_picture( SIColors.Color256( 256 , 256 , 256 , 256 ) , 24 ,
+	{
+		{
+			filename = "__base__/graphics/entity/accumulator/accumulator-charge.png" ,
+			priority = "high" ,
+			width = 90 ,
+			height = 100 ,
+			line_length = 6 ,
+			frame_count = 24 ,
+			blend_mode = "additive" ,
+			shift = util.by_pixel( 0 , -22 ) ,
+			hr_version =
+			{
+				filename = "__base__/graphics/entity/accumulator/hr-accumulator-charge.png" ,
+				priority = "high" ,
+				width = 178 ,
+				height = 206 ,
+				line_length = 6 ,
+				frame_count = 24 ,
+				blend_mode = "additive" ,
+				shift = util.by_pixel( 0 , -22 ) ,
+				scale = 0.5
+			}
+		}
+	} ) ,
 	charge_cooldown = 30 ,
 	charge_light = { intensity = 0.3 , size = 7 , color = SIColors.Color256( 255 , 255 , 255 ) } ,
-	discharge_animation = accumulator_discharge() ,
+	discharge_animation = accumulator_picture( SIColors.Color256( 256 , 256 , 256 , 256 ) , 24 ,
+	{
+		{
+			filename = "__base__/graphics/entity/accumulator/accumulator-discharge.png" ,
+			priority = "high" ,
+			width = 88 ,
+			height = 104 ,
+			line_length = 6 ,
+			frame_count = 24 ,
+			blend_mode = "additive" ,
+			shift = util.by_pixel( -2 , -22 ) ,
+			hr_version =
+			{
+				filename = "__base__/graphics/entity/accumulator/hr-accumulator-discharge.png" ,
+				priority = "high" ,
+				width = 170 ,
+				height = 210 ,
+				line_length = 6 ,
+				frame_count = 24 ,
+				blend_mode = "additive" ,
+				shift = util.by_pixel( -1 , -23 ) ,
+				scale = 0.5
+			}
+		}
+	} ) ,
 	discharge_cooldown = 60 ,
 	discharge_light = { intensity = 0.7 , size = 7 , color = SIColors.Color256( 255 , 255 , 255 ) } ,
 	working_sound =
